bee7cc104a42eece4b2b2105b41ed8f038d77095
Jboy422 <gitonly543@gmail.com>
2024-06-24 17:27:34 +0900

    Initial Commit

---
+++ CleanArchitectureSwift/CleanArchitectureSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureSwiftApp.swift
+//  CleanArchitectureSwift
+//
+//  Created by Jboy422 on 2024/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
